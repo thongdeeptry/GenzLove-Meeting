b9181c1d1f716a005a87e230009f187ce6525e1b
Ngô Thành Thông <ngothanhthongcode@gmail.com>
2022-11-05 20:09:24 +0700

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -19,7 +19,7 @@ const videoGrid = document.getElementById("video-grid");
 const myPeer = new Peer(undefined, {
   path: "/peerjs",
   host: "/",
-  port: "8080",
+  port: "443",
 });
 var Peer_ID;
 const myVideo = document.createElement("video");
